af86b6efea005242c4810cebae20d304fa000316
lujunsheng <lujunsheng@8d7e65aa-f446-4ea9-bd95-375d303c44a0>
2013-12-24 11:11:37 +0000

    commons-1.1.0

--- src/main/java/cn/qtone/concurrent/Callback.java
+++ src/main/java/cn/qtone/concurrent/Callback.java
@@ -1,6 +1,20 @@
 package cn.qtone.concurrent;
 
-public interface Callback {
+/**
+ * 多线程任务回调对象
+ * 
+ * @author 卢俊生, 2013-12-24
+ * @param <T> 任务类,Runnable子类
+ */
+public interface Callback<T extends Runnable> {
 
-	public abstract void callback(boolean result);
+	/**
+	 * 回调方法
+	 * 
+	 * @param taskName 任务名称
+	 * @param task 任务实例
+	 * @param result 任务结果
+	 * @see Result
+	 */
+	public abstract void callback(String taskName, T task, Result result);
 }
---
+++ src/main/java/cn/qtone/concurrent/Result.java
@@ -0,0 +1,15 @@
+package cn.qtone.concurrent;
+
+/**
+ * 多线程任务执行结果
+ * 
+ * @author 卢俊生, 2013-12-24
+ */
+public enum Result {
+	/** 正常 **/
+	ok,
+	/** 错误 **/
+	error,
+	/** 超时 **/
+	timeout
+}
