645d57677908d411c926c8da6c59532eba05d6a0
lujunsheng <lujunsheng@8d7e65aa-f446-4ea9-bd95-375d303c44a0>
2014-01-16 02:35:40 +0000

    commons-1.1.3 : add ExtMap

--- trunk/01-SourceCode/service/commons/src/main/java/cn/qtone/util/ExtMap.java
+++ trunk/01-SourceCode/service/commons/src/main/java/cn/qtone/util/ExtMap.java
@@ -4,6 +4,7 @@
 package cn.qtone.util;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author 卢俊生, 2014-1-16
@@ -11,6 +12,18 @@ import java.util.HashMap;
 public class ExtMap extends HashMap<String, Object> {
 
 	private static final long serialVersionUID = 1L;
+
+	public ExtMap() {
+		super();
+	}
+
+	public ExtMap(int initialCapacity) {
+		super(initialCapacity);
+	}
+
+	public ExtMap(Map<String, Object> map) {
+		super(map);
+	}
 
 	@SuppressWarnings("unchecked")
 	public <T> T get(String key, Class<T> clazz) {
