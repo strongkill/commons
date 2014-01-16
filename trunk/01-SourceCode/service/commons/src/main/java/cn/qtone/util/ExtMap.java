4587a53a5dfe3971575ab72d7be6de356d3b6068
lujunsheng <lujunsheng@8d7e65aa-f446-4ea9-bd95-375d303c44a0>
2014-01-16 03:04:25 +0000

    commons-1.1.3 : add ExtMap

--- trunk/01-SourceCode/service/commons/src/main/java/cn/qtone/util/ExtMap.java
+++ trunk/01-SourceCode/service/commons/src/main/java/cn/qtone/util/ExtMap.java
@@ -7,6 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * HashMap<String, Object>扩展,支持
  * @author 卢俊生, 2014-1-16
  */
 public class ExtMap extends HashMap<String, Object> {
@@ -25,13 +26,28 @@ public class ExtMap extends HashMap<String, Object> {
 		super(map);
 	}
 
+	/**
+	 * 获取指定键所映射的值(忽略大小写,泛型)
+	 * <pre>如Map中存在多个可映射的项,其优先级为 1.完全匹配>2.全大写匹配>3.全小写匹配>4.其它</pre>
+	 * @param key 键
+	 * @param clazz 值类型
+	 * @return 指定键所映射的值
+	 */
 	@SuppressWarnings("unchecked")
 	public <T> T get(String key, Class<T> clazz) {
 		Object obj = null;
-		for (java.util.Map.Entry<String, Object> entry : entrySet()) {
-			if (entry.getKey().equalsIgnoreCase(key)) {
-				obj = entry.getValue();
-				break;
+		if (containsKey(key)) {
+			obj = get(key);
+		} else if (containsKey(key.toUpperCase())) {
+			obj = get(key.toUpperCase());
+		} else if (containsKey(key.toLowerCase())) {
+			obj = get(key.toLowerCase());
+		} else {
+			for (java.util.Map.Entry<String, Object> entry : entrySet()) {
+				if (entry.getKey().equalsIgnoreCase(key)) {
+					obj = entry.getValue();
+					break;
+				}
 			}
 		}
 		return (T) obj;
