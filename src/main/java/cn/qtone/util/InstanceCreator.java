14e10e057e038ccbace85861e34e945f9ab753bc
lujunsheng <lujunsheng@8d7e65aa-f446-4ea9-bd95-375d303c44a0>
2014-02-27 03:44:59 +0000

    commons-1.1.5

--- src/main/java/cn/qtone/util/InstanceCreator.java
+++ src/main/java/cn/qtone/util/InstanceCreator.java
@@ -3,6 +3,42 @@ package cn.qtone.util;
 public class InstanceCreator {
 
 	@SuppressWarnings("unchecked")
+	public static final <T> T create(Class<T> clazz) {
+		Object object = null;
+		try {
+			object = clazz.newInstance();
+		} catch (Exception e) {
+			throw new RuntimeException("create new instance failed!", e);
+		}
+
+		return (T) object;
+	}
+
+	@SuppressWarnings("unchecked")
+	public static final <T> T create(Class<T> clazz, Class<?> parameterType, Object initarg) {
+		Object object = null;
+		try {
+			object = clazz.getConstructor(parameterType).newInstance(initarg);
+		} catch (Exception e) {
+			throw new RuntimeException("create new instance failed!", e);
+		}
+
+		return (T) object;
+	}
+
+	@SuppressWarnings("unchecked")
+	public static final <T> T create(Class<T> clazz, Class<?>[] parameterTypes, Object[] initargs) {
+		Object object = null;
+		try {
+			object = clazz.getConstructor(parameterTypes).newInstance(initargs);
+		} catch (Exception e) {
+			throw new RuntimeException("create new instance failed!", e);
+		}
+
+		return (T) object;
+	}
+
+	@SuppressWarnings("unchecked")
 	public static final <T> T create(String className) {
 		Object object = null;
 		try {
